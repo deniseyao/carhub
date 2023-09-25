1a6ca6e23bec5d0be68634a0dd12d91bf77f930c
Denise Yao <denisemejiayao10@gmail.com>
2023-09-26 00:08:43 +0800

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  Navbar,
+  Hero,
+  CustomButton,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
